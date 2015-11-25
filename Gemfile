f1a9820f6809c9fa6cc5b889e90094692ae004fd
Matthew Smith <web.matthew.it@gmail.com>
2015-11-25 17:36:00 +0000

    added association and user secure password

--- Gemfile
+++ Gemfile
@@ -14,6 +14,9 @@ gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 gem 'bootstrap-sass', '~> 3.3.5'
+gem 'bcrypt', '~> 3.1.7'
+
+
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ActiveRecord::Base
     
+    belongs_to :user
+    
     validates :description, presence: true, length: { minimum: 30, maximum: 400};
     validates :title, presence: true, length: {minimum: 10, maximum: 50}
+    validates :user_id, presence: true
     
 end
