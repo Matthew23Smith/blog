8b1050f3defc2ed3f39fb8014dda7e0bae40169c
Matthew Smith <web.matthew.it@gmail.com>
2015-11-26 22:11:33 +0000

    added admin user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
     has_secure_password
-    has_many :articles;
+    has_many :articles, dependent: :destroy
+    
     before_save { self.email = email.downcase }
     
    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
